4675c6246923b3ccd9f316bdffbaa84402def15f
Felipe Enne <felipeenne@gmail.com>
2022-03-02 10:39:24 -0300

    preload

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'src/models/appdata.dart';
 import 'src/pages/home.dart';
+import 'src/pages/preload.dart';
 
 void main() => runApp(MultiProvider(
     providers: [ChangeNotifierProvider(create: (context) => AppData())],
@@ -13,8 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {'/home': (context) => HomePage()},
-      initialRoute: '/home',
+      routes: {
+        '/preload': (context) => PreloadPage(),
+        '/home': (context) => HomePage()
+      },
+      initialRoute: '/preload',
     );
   }
 }
